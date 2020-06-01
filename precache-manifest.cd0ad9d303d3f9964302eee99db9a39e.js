80289e092aa1c856ad0fc6e95b2e13ea2182dc9e
Caleb Brochu <caleb.brochu@gmail.com>
2020-05-31 18:02:37 -0700

    Updates

---
+++ precache-manifest.cd0ad9d303d3f9964302eee99db9a39e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc06fe0f0d79e3e889714f96e74278e",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "a7b88b3326def93eb178",
+    "url": "/React_Portfolio/static/css/main.ba60d4b0.chunk.css"
+  },
+  {
+    "revision": "583e0220641408c33bd1",
+    "url": "/React_Portfolio/static/js/2.6d38ab3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Portfolio/static/js/2.6d38ab3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b88b3326def93eb178",
+    "url": "/React_Portfolio/static/js/main.df1f0c05.chunk.js"
+  },
+  {
+    "revision": "9aed4dc40bccf8423800",
+    "url": "/React_Portfolio/static/js/runtime-main.8167d654.js"
+  },
+  {
+    "revision": "6740a958cb22f293a11b0ff4f2382b88",
+    "url": "/React_Portfolio/static/media/Burgers.6740a958.JPG"
+  },
+  {
+    "revision": "336f51a832d44f808cc1757585a3b7d9",
+    "url": "/React_Portfolio/static/media/DayPlanner.336f51a8.png"
+  },
+  {
+    "revision": "156bbc6a480ddc3aeb8318eebeb34bd8",
+    "url": "/React_Portfolio/static/media/Password-generator.156bbc6a.JPG"
+  },
+  {
+    "revision": "c339c31cb0f30aa37223e0f8f483aa68",
+    "url": "/React_Portfolio/static/media/Weather_Dashboard.c339c31c.JPG"
+  },
+  {
+    "revision": "d740751d691f6bb456e043fbc5e8e166",
+    "url": "/React_Portfolio/static/media/forage.d740751d.JPG"
+  },
+  {
+    "revision": "dba5c2791f33d2b0b00edb29e32068d4",
+    "url": "/React_Portfolio/static/media/head_shot.dba5c279.jfif"
+  },
+  {
+    "revision": "55d3746927d5eb5cfa5fcf9dec2182a7",
+    "url": "/React_Portfolio/static/media/hiking.55d37469.JPG"
+  },
+  {
+    "revision": "adde369ac6b56222128b6f4cf7f2cdda",
+    "url": "/React_Portfolio/static/media/suitcase.adde369a.JPG"
+  }
+]);
